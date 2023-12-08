6031994207eb52b81663a1d4b5c610cd9570bc78
Jacko Dirks <jacko.dirks@gmail.com>
2023-12-08 22:14:16 +0100

    day8, polished

--- day8/part1.py
+++ day8/part1.py
@@ -1,34 +1,22 @@
 import re
+import itertools
 
 if __name__ == "__main__":
     with open("input") as file:
         lines = [line.rstrip() for line in file]
-
-    lines_iter = iter(lines)
-    instructions = next(lines_iter)
-    next(lines_iter)
-
-    node_dict = {}
-
-    for line in lines_iter:
-        match = re.match("([^\s]+)\s+=\s+\(([^\s]+),\s+([^\s]+)\)", line)
-        node_dict[match.group(1)] = (match.group(2), match.group(3))
-
+    instructions = lines[0]
+    node_dict = {match.group(1): (match.group(2), match.group(3)) for match in [re.match("([^\s]+)\s+=\s+\(([^\s]+),\s+([^\s]+)\)", line) for line in lines[2:]]}
     node = node_dict["AAA"]
-
-    stop = False
     step_count = 0
-    while not stop:
-        for instruction in instructions:
-            if instruction == "L":
-                key = node[0]
-            else:
-                key = node[1]
-            node = node_dict[key]
-            step_count += 1
-            if key == "ZZZ":
-                stop = True
-                break
+    for instruction in itertools.cycle(instructions):
+        if instruction == "L":
+            key = node[0]
+        else:
+            key = node[1]
+        node = node_dict[key]
+        step_count += 1
+        if key == "ZZZ":
+            break
     print(step_count)
 
 
--- day8/part2.py
+++ day8/part2.py
@@ -1,5 +1,6 @@
 import re
 import math
+import itertools
 
 def determine_cycle(instructions, node_dict, start_node_key):
     index_node_pairs = {i : [] for i in range(len(instructions))}
@@ -7,24 +8,21 @@ def determine_cycle(instructions, node_dict, start_node_key):
     start_node = node_dict[start_node_key]
     cycle_length = 0
     end_counts = []
-    stop = False
-    while not stop:
-        for index in range(len(instructions)):
-            instruction = instructions[index]
-            if instruction == "L":
-                start_node_key = start_node[0]
-            else:
-                start_node_key = start_node[1]
-            start_node = node_dict[start_node_key]
-            cycle_length += 1
-            index_cycle_step_pairs[index].append(cycle_length)
-            if start_node_key[-1] == "Z":
-                end_counts.append(cycle_length)
-            if start_node_key in index_node_pairs[index]:
-                relevant_set = index_cycle_step_pairs[index]
-                stop = True
-                break
-            index_node_pairs[index].append(start_node_key)
+    for index in itertools.cycle(range(len(instructions))):
+        instruction = instructions[index]
+        if instruction == "L":
+            start_node_key = start_node[0]
+        else:
+            start_node_key = start_node[1]
+        start_node = node_dict[start_node_key]
+        cycle_length += 1
+        index_cycle_step_pairs[index].append(cycle_length)
+        if start_node_key[-1] == "Z":
+            end_counts.append(cycle_length)
+        if start_node_key in index_node_pairs[index]:
+            relevant_set = index_cycle_step_pairs[index]
+            break
+        index_node_pairs[index].append(start_node_key)
     return end_counts[0], relevant_set[0], relevant_set[-1]
 
 if __name__ == "__main__":
@@ -36,6 +34,5 @@ if __name__ == "__main__":
     node_dict = {match.group(1): (match.group(2), match.group(3)) for match in [re.match("([^\s]+)\s+=\s+\(([^\s]+),\s+([^\s]+)\)", line) for line in lines_iter]}
     active_nodes = [k for k in node_dict.keys() if k[-1] == "A"]
     data_points = [(determine_cycle(instructions, node_dict, node)) for node in active_nodes]
-    #values = [end_point for end_point, _, _ in data_points]
     steps = [cycle_end - cycle_start for _, cycle_start, cycle_end in data_points]
     print(math.lcm(*steps))
